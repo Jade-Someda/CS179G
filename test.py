0421c70ad77ff3a3780776f597464ae6a17e2603
tcarillo <tanya.carrillo707@gmail.com>
2026-02-03 21:52:53 -0800

    Update cleaning script

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, when, trim, lower, try_to_timestamp, lit
+import time
 
-
+start = time.time()
 
 spark = SparkSession.builder \
     .appName("ChicagoCrimeClean") \
@@ -12,8 +13,11 @@ crime_df = spark.read.option("header", True).csv("/Users/nehagutlapalli/Download
 #crime_df.printSchema()
 #crime_df.show(5)
 
-crime_df = crimedf.toDF(
-    *[c.lower().replace(" ", "") for c in crime_df.columns]
+original_rows = crime_df.count()
+original_cols = len(crime_df.columns)
+
+crime_df = crime_df.toDF(
+        *[c.lower().replace(" ", "_") for c in crime_df.columns]
 )
 
 crime_df = crime_df.dropna(
@@ -45,12 +49,29 @@ crime_df = crime_df.withColumn(
     .otherwise(None)
 )
 
+
 crime_df = crime_df.dropDuplicates(["id"])
 
-
+crime_df = crime_df.drop(
+        "case_number", "block", "iucr", "beat", "district", "ward",
+        "fbi_code", "x_coordinate", "y_coordinate", "updated_on", "location"
+)
 crime_df.write \
      .mode("overwrite") \
     .parquet("/Users/nehagutlapalli/Downloads/clean_chicago_crime")
 
+clean_rows = crime_df.count()
+clean_cols = len(crime_df.columns)
+print("Rows before:", original_rows)
+print("Rows after:", clean_rows)
+print("Columns before:", original_cols)
+print("Columns after:", clean_cols)
+end = time.time()
+print("Cleaning runtime:", end-start, "seconds")
 
+
+crime_df.describe(["x","y"]).show()
+crime_df.groupBy("arrest").count.show()
+crime_df.groupBy("domestic").count.show()
+crime_df.select("primary_type").distinct().count()
 spark.stop()
